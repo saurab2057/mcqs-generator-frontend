cb06b95a1d7370f5cc623a938157aa48b9de9af0
Saurab Khatiwoda <saurabkhatiwoda@gmail.com>
2025-09-17 21:05:13 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server:{
-    allowedHosts:['cb7b5e08a37e.ngrok-free.app']
-    }
 })
