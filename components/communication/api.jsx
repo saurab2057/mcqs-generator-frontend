38e42d7c5a0ede30e1068cab92ba238a7af774db
Saurab Khatiwoda <saurabkhatiwoda@gmail.com>
2025-09-17 21:04:48 +0545

    Update api.jsx

--- components/communication/api.jsx
+++ components/communication/api.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Create Axios instance
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000", // base URL of backend
+  baseURL: "https://mcqs-generator-backend.onrender.com", // base URL of backend
   withCredentials: true, // send cookies/auth with requests
   headers: {
     "Content-Type": "application/json",
